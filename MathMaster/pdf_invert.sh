085fe2dfdf07423959d91cd0e13321d96fcb5894
japseow <japseow@gmail.com>
2019-09-22 21:59:40 +0800

    calc cheats

--- MathMaster/pdf_invert.sh
+++ MathMaster/pdf_invert.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-bin_dir="{1:-C:\Users\guzma\Downloads\PortableInstallations\texlive\tlpkg\tlgs\bin\gswin32c.exe}"
+bin_dir="${1:-/c/Users/guzma/Downloads/PortableInstallations/texlive/tlpkg/tlgs/bin/gswin32c.exe}"
 echo "Enter input directory: " && read in_dir
 out_dir="${in_dir}/inverted"
 
 cd "${in_dir}" && (mkdir "${out_dir}" || echo "Could not make a new directory (${out_dir})")
 ls . | grep -E "\.pdf$" | \
-  xargs -I{} "${bin_dir}" \
+  xargs -I{} "${bin_dir}" -sSUBSTFONT=Arial-BoldMT \
     -o "${out_dir}/{}" -sDEVICE=pdfwrite \
     -c "{1 exch sub}{1 exch sub}{1 exch sub}{1 exch sub} setcolortransfer" -f "{}"
 echo "Press enter to continue: " && read
