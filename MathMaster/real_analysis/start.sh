90363f1b277522252595f4028bf840913a60dd48
John Paul Guzman <japseow@gmail.com>
2020-01-19 03:03:15 +0800

    f

--- MathMaster/real_analysis/start.sh
+++ MathMaster/real_analysis/start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 default_bookmark="OrderR"
 bookmark=${1:-${default_bookmark}}
-firefox analysis.pdf#${bookmark} &
+#firefox analysis.pdf#${bookmark} &
+qpdfview analysis.pdf &
 qpdfview rudin/RudinW.PrinciplesOfMathematicalAnalysis3e1976600Dpi.pdf &
