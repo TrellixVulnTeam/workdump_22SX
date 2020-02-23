1afcc3f078575a3bac0be36076dcc2ab74bc36b6
japseow <japseow@gmail.com>
2020-02-24 01:53:17 +0800

    f

--- MathMaster/real_analysis/build.sh
+++ MathMaster/real_analysis/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Use as: { "keys": [ "ctrl+h" ], "command": "exec", "args": { "shell_cmd": "\"C:\\Program Files\\Git\\usr\\bin\\bash.exe\" build.sh" } }
+# Use as: { "keys": [ "ctrl+alt+b" ], "command": "exec", "args": { "shell_cmd": "\"C:\\Program Files\\Git\\usr\\bin\\bash.exe\" build.sh" } }
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 original_path="analysis.tex"
 transpile_path="__analysis.tex"
