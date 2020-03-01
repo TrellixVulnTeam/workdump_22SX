3120c6afb9df047957f76fa94bff2dba66fa790d
japseow <japseow@gmail.com>
2020-03-01 21:28:18 +0800

    f

--- MathMaster/real_analysis/start.sh
+++ MathMaster/real_analysis/start.sh
@@ -9,5 +9,6 @@ else
 fi
 
 "${viewer}" __analysis.pdf &
-"${viewer}" rudin/RudinW.PrinciplesOfMathematicalAnalysis3e1976600Dpi.pdf &
+#"${viewer}" rudin/RudinW.PrinciplesOfMathematicalAnalysis3e1976600Dpi.pdf &
 "${viewer}" dlsu/linear_algebra/smallpdf.com_la.pdf &
+"${viewer}" dlsu/linear_algebra/LINEALGEC1.pdf
