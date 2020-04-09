9d6fc903ac9d6ee2e06c4904c3e5f6381b1d27aa
John Paul Guzman <jpguzman@viyahe.com>
2020-04-10 00:50:28 +0800

    f

--- MathMaster/real_analysis/start.sh
+++ MathMaster/real_analysis/start.sh
@@ -8,9 +8,9 @@ else
     viewer="qpdfview"
 fi
 
+subl analysis.tex &
 "${viewer}" __analysis.pdf &
 #"${viewer}" rudin/RudinW.PrinciplesOfMathematicalAnalysis3e1976600Dpi.pdf &
 #"${viewer}" dlsu/linear_algebra/smallpdf.com_la.pdf &
-"${viewer}" dlsu/linear_algebra/LINEALGLEC1.pdf &
+#"${viewer}" dlsu/linear_algebra/LINEALGLEC1.pdf &
 #"${viewer}" dlsu/linear_algebra/\(Undergraduate\ Texts\ in\ Mathematics\)\ Sheldon\ Axler\ \(auth.\)\ -\ Linear\ Algebra\ Done\ Right-Springer\ International\ Publishing\ \(2015\).pdf &
-subl analysis.tex &
