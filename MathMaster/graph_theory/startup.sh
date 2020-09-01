a365763dd6f077efa42241ea9d2239ff89cdffa4
John Paul Guzman <jpguzman@viyahe.com>
2020-09-01 08:48:01 +0800

    fixup! 4.3

--- MathMaster/graph_theory/startup.sh
+++ MathMaster/graph_theory/startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-evince __analysis.pdf &
-evince Douglas\ B.\ West\ -\ Introduction\ to\ Graph\ Theory\ \(2nd\ Edition\)\(With\ Solution\ Manual\)\ \ -Prentice\ Hall\ \(2000\).pdf &
-evince GRAPTHE2020LEC01.pdf &
-evince GRAPTHE2020LEC02.pdf &
+#evince graph_theory.pdf &
+#evince Douglas\ B.\ West\ -\ Introduction\ to\ Graph\ Theory\ \(2nd\ Edition\)\(With\ Solution\ Manual\)\ \ -Prentice\ Hall\ \(2000\).pdf &
+#evince GRAPTHE2020LEC01.pdf &
+#evince GRAPTHE2020LEC02.pdf &
+evince 1309.0225.pdf &
+evince 1309.0225.pdf &
