dc5fcbb2c49817142c896ab12e3e7c9dc218105c
John Paul Guzman <jpguzman@viyahe.com>
2020-10-05 13:57:23 +0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! f

--- tools/act_util.py
+++ tools/act_util.py
@@ -7,6 +7,7 @@ list_str = run(list_cmd)
 workflows = re.findall("[a-z-]{2,}", list_str)
 act_all_list = [f"act -j {workflow} -v" for workflow in workflows]
 print(" && ".join(act_all_list))
+print(" && ".join([a for a in act_all_list if "pydocstyle" not in a]))
 # act_all_cmd = ["sh", "-c", " && ".join(act_all_list)]
 # run_all_str = run(act_all_cmd)
 # print(run_all_str)
