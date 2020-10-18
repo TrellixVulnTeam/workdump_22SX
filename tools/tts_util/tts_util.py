74fab6ab72b596d44b5ac0a0effc3c38a9629b7c
japseow <japseow@gmail.com>
2020-10-19 06:16:45 +0800

    f

--- tools/tts_util/tts_util.py
+++ tools/tts_util/tts_util.py
@@ -24,7 +24,7 @@ class TTSUtil:
             text = self.text_interface.retrieve_text()
             self.tts.speak(text)
             # Note: you need manually deactivate due to a pyttsx3 bug
-            self.is_active = False
+            # self.is_active = False
 
     def _on_deactivate(self):
         if self.is_active:
