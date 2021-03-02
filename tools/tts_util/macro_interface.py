1db11411664947a3aea28aa16685515556fce61f
John Paul Guzman <jpguzman@viyahe.com>
2021-03-02 19:47:11 +0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! p25 exercise

--- tools/tts_util/macro_interface.py
+++ tools/tts_util/macro_interface.py
@@ -9,10 +9,15 @@ logger.setLevel(logging.DEBUG)
 
 class MacroInterface:
     def __init__(self):
-        self.input_wait = 0.02
+        self.input_wait = 0.01
+        self.load_wait = 0.5
         self.keyboard_controller = keyboard.Controller()
         self.mouse_controller = mouse.Controller()
-        self.wait = lambda: sleep(self.input_wait)
+
+
+    def wait(self, duration=None):
+        duration = self.input_wait if duration is None else duration
+        sleep(duration)
 
     def _click(self, button, count=1):
         for _ in range(count):
@@ -30,14 +35,19 @@ class MacroInterface:
         self._press(keyboard.Key.down, 7)
         self._press(keyboard.Key.enter)
 
-    def add_edit_highlight(self):
-        logger.info("attempting: add_edit_highlight")
-        self.add_highlight()
+    def edit_highlight(self):
+        logger.info("attempting: edit_highlight")
         self._click(mouse.Button.right)
         self._press(keyboard.Key.down, 6)
         self._press(keyboard.Key.enter)
         self._press(keyboard.Key.down)
         self._press(keyboard.Key.enter)
+
+    def add_edit_highlight(self):
+        logger.info("attempting: add_edit_highlight")
+        self.add_highlight()
+        self.wait(self.load_wait)
+        self.edit_highlight()
 
     def toggle_highlight(self):
         logger.info("attempting: toggle_highlight")
